4dc1ca03305e2a7856711548b2142f77dc5408db
roum02 <roum02@ewhain.net>
2024-09-19 23:58:54 +0900

    Add contenteditable component [feature/contenteditable]

---
+++ src/component/contenteditable.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContentEditableEditor = () => {
+    return (
+        <div id="contenteditable" contentEditable={true}>
+            hello this is a test
+        </div>
+    )
+}
+
+export default ContentEditableEditor;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ContentEditableEditor from "./component/contenteditable"; // 컴포넌트 이름이 정확한지 확인
+
+ReactDOM.render(<ContentEditableEditor />, document.getElementById("root"));
